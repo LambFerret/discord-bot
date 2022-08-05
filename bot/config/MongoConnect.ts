c2867ea372d7832e0a871e785ee315a0d2ab093c
LambFerret <dignrjrl12@gmail.com>
2022-08-06 00:42:57 +0900

    fix: package, log

--- bot/config/MongoConnect.ts
+++ bot/config/MongoConnect.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
 import { CONFIG } from "./Config";
 const uri = CONFIG.MONGO_URI as string;
+console.log(uri);
+
 export const client = new MongoClient(uri).db("test");
 
 
